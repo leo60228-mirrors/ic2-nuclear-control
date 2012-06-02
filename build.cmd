03565e192e79560a00fbff3cbfee8196d9dc680f
Oleg Rmanovsky aka Shedar <shedarhome@gmail.com>
2012-06-02 15:39:30 +0300

    build script fix

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 xcopy /Q /S /Y client\*  ..\mcp\src\minecraft\
 xcopy /Q /S /Y common\*  ..\mcp\src\minecraft\
 xcopy /Q /S /Y server\*  ..\mcp\src\minecraft_server\
-xcopy /Q /S /Y common\*  ..\mcp\minecraft_server\
+xcopy /Q /S /Y common\*  ..\mcp\src\minecraft_server\
 
 cd ..\mcp
 runtime\bin\python\python_mcp runtime\recompile.py && runtime\bin\python\python_mcp runtime\reobfuscate.py
