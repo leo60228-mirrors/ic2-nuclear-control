acaadd827766c8cfddba36a785782d6638145bbf
Oleg Rmanovsky aka Shedar <shedarhome@gmail.com>
2012-07-11 12:50:13 +0300

    mcmod.info file added

--- pack.cmd
+++ pack.cmd
@@ -2,5 +2,5 @@
 
 del builds\IC2NuclearControl-dev.zip
 del builds\IC2NuclearControl-server-dev.zip
-"C:\Program Files\7-Zip\7z" a builds\IC2NuclearControl-dev.zip ..\mcp\reobf\minecraft\*  %~dp0client\img %~dp0client\sound
-"C:\Program Files\7-Zip\7z" a builds\IC2NuclearControl-server-dev.zip ..\mcp\reobf\minecraft_server\* 
+"C:\Program Files\7-Zip\7z" a builds\IC2NuclearControl-dev.zip ..\mcp\reobf\minecraft\*  %~dp0client\img %~dp0client\sound %~dp0common\mcmod.info
+"C:\Program Files\7-Zip\7z" a builds\IC2NuclearControl-server-dev.zip ..\mcp\reobf\minecraft_server\* %~dp0common\mcmod.info
