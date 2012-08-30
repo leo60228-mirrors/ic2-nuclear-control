f0f3c13c0a5c7dc3ddb4796f7121cf52c3828237
Oleg Romanovsky aka Shedar <shedarhome@gmail.com>
2012-08-30 03:17:55 +0300

    info panel: enable labels by default

--- common/net/minecraft/src/nuclearcontrol/TileEntityInfoPanel.java
+++ common/net/minecraft/src/nuclearcontrol/TileEntityInfoPanel.java
@@ -217,7 +217,8 @@ public class TileEntityInfoPanel extends TileEntity implements
         facing = 0;
         prevFacing = 0;
         prevRotation = 0;
-        rotation = 0;        
+        rotation = 0;
+        showLabels = true;
     }
     
     @Override
